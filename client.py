31e4187c0723bfe448c90311dca8fb1747fc89cc
Ranit <161700424+ranit004@users.noreply.github.com>
2024-08-21 17:45:33 +0530

    client.py

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 from openai import OpenAI
 client = OpenAI(
-    api_key="sk-None-5WvwlXJ5i3FWnU6c3ZldT3BlbkFJylLhG6NjQ3jwky5vkTdj"
+    api_key="your own api key"
 )
 
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-        {"role": "user", "content": "what is programming."}
+        {"role": "user", "content": "what is python programming"}
   ]
 )
 
